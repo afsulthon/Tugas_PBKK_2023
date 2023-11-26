914254c4dc23417b11a7ad8a492cf0cfac62ea11
Akmal <akmalsulthonf@gmail.com>
2023-11-26 11:50:29 +0700

    Update Tugas Pertemuan 10

---
+++ Tugas Pertemuan 10/ci4_hello/app/Views/hello.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello World Page</title>
+  <link rel="stylesheet" href="<?= base_url('/style.css'); ?>">
+</head>
+
+<body>
+  <div class="container">
+    <h1>Hello, World!</h1>
+    <p>Saya sedang belajar menggunakan framework CodeIgniter.</p>
+  </div>
+</body>
+
+</html>
